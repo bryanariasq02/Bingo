8f784999bfacd1dec8f4680e4023d0d8f0544533
Sneyder Martínez Caicedo <sneider.caicedo@gmail.com>
2022-09-17 03:43:10 +0000

    Content ballot number

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 Xo = (new Date()).getTime() / 1000;
-console.log(parseInt(Xo))
+// console.log(parseInt(Xo))
 a = 1103515245
 c = 12345
 m = 32768
@@ -13,9 +13,9 @@ cantidad = 0
 iteraciones = 0
 stringCant = String(numeros)
 len = stringCant.length
-console.log(len)
+// console.log(len)
 base = Math.pow(10,len+1)
-console.log(base)
+// console.log(base)
 
 while(cantidad<numeros){
     Xn = (a*Xo + c) % m
@@ -36,23 +36,30 @@ console.log("Posibilidades: ", total)
 
 function juega(pArray) {
 
-    console.log("Vamos a juga")
+    // console.log("Vamos a juga")
 
     let index = 0;
+    let numero;
 
     for(index; index <= pArray.length; index++) {
 
-        console.log("La balota lanzada es: ", pArray[index])
+        console.log("La balota encontrada es: ", pArray[index])
+        numero = pArray[index]
         pArray.shift()
         break
     }
 
     if(pArray.length === 0) {
+
         console.log("Fin de juego")
         const boton = document.getElementById('prueba');
         boton.disabled = true;
-        boton.style.backgroundColor = '#F00A0A'
-        boton.style.color = '#fff'
+        boton.style.backgroundColor = '#F00A0A';
+        boton.style.color = '#d8d0ce';
+        boton.style.borderColor = '#fff';
     }
+
+    let divBalota = document.querySelector('#balota');
+    divBalota.textContent = numero
 
 }
